84f3aae26c737dee9a64ef21763541d6643d9b1f
xiaofan <ningfengzeng@gmail.com>
2024-12-25 21:30:56 +0800

    feat: remove environment and license files; add authentication guard to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,18 @@
 // import { createRouter, createWebHashHistory } from 'vue-router/auto'
 import { createRouter, createWebHistory } from 'vue-router/auto'
+import { useLocalCache } from '~/hook'
+
+const { getCache } = useLocalCache()
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !getCache('token'))
+    next('/login')
+  else
+    next()
 })
 
 export default router
